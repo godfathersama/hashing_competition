cc6483638cca89b0141177344c4291a2a493b424
Michael Williams <mich@elwillia.ms>
2014-10-02 12:49:42 -0500

    Add methods to score attempts

--- app/controllers/hashes_controller.rb
+++ app/controllers/hashes_controller.rb
@@ -1,4 +1,34 @@
 class HashesController < ApplicationController
   def attempt
+    key = params[:key]
+    attempt = params[:attempt]
+
+    @user = User.where(:hash => key).first
+
+    new_score = self.score_attempt(attempt)
+
+    if @user.score < new_score
+      @user.score = new_score
+      if !@user.save
+        @error = 'ERROR SAVING NEW SCORE: TRY AGAIN!'
+      end
+    end
+  end
+
+  private
+  def score_attempt(attempt)
+    digest = Digest::SHA256.hexdigest(attempt)
+    cout_leading_zeros(digest)
+  end
+
+  def count_leading_zeros(digest)
+    zeros = 0
+
+    digest.each_char do |c|
+      break if c != '0'
+      zeros += 1
+    end
+
+    zeros
   end
 end
