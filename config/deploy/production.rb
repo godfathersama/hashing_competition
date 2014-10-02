26c34fece973ff2329f6b4237e4ea1415d0f543d
Michael Williams <mich@elwillia.ms>
2014-10-02 15:10:10 -0500

    Set up deployment configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+#role :app, %w{deploy@104.131.205.134}
+#role :web, %w{deploy@104.131.205.134}
+#role :db,  %w{deploy@104.131.205.134}
 
 
 # Extended Server Syntax
@@ -15,8 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-
+server '104.131.205.134', user: 'deploy', roles: %w{web app db}
 
 # Custom SSH Options
 # ==================
@@ -43,3 +42,4 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 #     auth_methods: %w(publickey password)
 #     # password: 'please use keys'
 #   }
+
