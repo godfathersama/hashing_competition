29fd342314e777b9e2e4d0d55397c9acc2db7537
Michael Williams <mich@elwillia.ms>
2014-10-02 12:52:49 -0500

    Add registration email action

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,3 +1,10 @@
 class Notifier < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "noreply@elwillia.ms"
+
+  def send_registration_email(user)
+    @user = user
+
+    mail( to: @user.email,
+      subject: 'Thanks for signing up!' )
+  end
 end
