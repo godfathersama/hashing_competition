6b25bcf645e7b8daf789c0dab7dd010abd6c694a
Michael Williams <mich@elwillia.ms>
2014-10-03 09:47:18 -0500

    Add relationship between user and attempt model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   before_create :assign_unique_key
 
   validates_presence_of :username, on: :create
-  validates_presence_of :email, on: :create 
+  validates_presence_of :email, on: :create
+
+  has_many :attempts
 
   def assign_unique_key
     self.key = SecureRandom.hex
